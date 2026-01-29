fbd0cb975fba6b00e679c19e4ea761be833d6824
Michael <28543330+Schello805@users.noreply.github.com>
2026-01-29 19:20:03 +0100

    main

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development" || process.env.DISABLE_PWA === "1",
   workboxOptions: {
     disableDevLogs: true,
   },
